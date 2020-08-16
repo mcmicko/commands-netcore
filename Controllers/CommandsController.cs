64a1c9334818478f4181c430c02fc4284e4c48c4
mcmicko <mcmzrc@gmail.com>
2020-08-16 22:10:30 +0200

    implement method PATCH

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using Commander.Data;
 using Commander.Dtos;
 using Commander.Model;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
@@ -58,6 +59,28 @@ namespace Commander.Controllers
         return NotFound();
 
       _mapper.Map(commandUpdateDto, commandModelFromRepo);
+      _repository.UpdateCommand(commandModelFromRepo);
+      _repository.SaveChanges();
+
+      return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public ActionResult PartialCommandUpdate(int id, JsonPatchDocument<CommandUpdateDto> patchDoc)
+    {
+      var commandModelFromRepo = _repository.GetCommandById(id);
+
+      if (commandModelFromRepo == null)
+        return NotFound();
+
+      var commandToPatch = _mapper.Map<CommandUpdateDto>(commandModelFromRepo);
+      patchDoc.ApplyTo(commandToPatch, ModelState);
+
+      if (!TryValidateModel(commandToPatch))
+        return ValidationProblem(ModelState);
+
+      _mapper.Map(commandToPatch, commandModelFromRepo);
+
       _repository.UpdateCommand(commandModelFromRepo);
       _repository.SaveChanges();
 
--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace Commander
 {
@@ -30,9 +31,13 @@ namespace Commander
     {
       services.AddDbContext<CommanderContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("commanderConnection")));
 
-      services.AddControllers();
+      services.AddControllers().AddNewtonsoftJson(s =>
+      {
+        s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+      });
 
       services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
       services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
     }
 
